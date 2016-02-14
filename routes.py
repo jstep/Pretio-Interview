824bf600c0db42f5ea2fb1cab6372fbce6ac2446
James <jstephaniuk@gmail.com>
2016-02-13 16:34:05 -0800

    Debugging

--- routes.py
+++ routes.py
@@ -23,20 +23,19 @@ def serve_ad():
         ip = request.environ['REMOTE_ADDR']
         # ip = "104.142.123.45"
 
-    if ip != "127.0.0.1":
-        cc = "&country_code={}".format(gi.country_code_by_addr(ip))
-    else:
-        cc = "&country_code=US"
-    # cc = 'country_code=CA'  # TODO: Delete when done testing and deployed.
+    cc = "&country_code={}".format(gi.country_code_by_addr(ip))
+    cc = "&country_code=US"
+
+    print ip + "\n\n\n\n"+ cc 
 
     # HTTP Post.
     base_url = "https://offers.pretio.in/publishers/{}/api/?user_id=12345{}".format(API_KEY, cc)
     print base_url + "\n\n\n"
     headers = {'content-type': 'application/json'}
     response = requests.post(base_url, headers=headers)
-    url_req = response.json()['url']
+    url = response.json()['url']
 
-    return render_template('ad.html', url=url_req)
+    return render_template('ad.html', url=url)
 
 if __name__ == '__main__':
     app.run(debug=True)
