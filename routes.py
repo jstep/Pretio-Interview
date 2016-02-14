734ccb1cbe6465724ed211d75f6c28bda69f1853
James <jstephaniuk@gmail.com>
2016-02-13 19:55:53 -0800

    Remove IP debugging

--- routes.py
+++ routes.py
@@ -15,21 +15,19 @@ def serve_ad():
     # TODO: Obscure api key with a config file.
     API_KEY = "c00d0814f0b548c68817473b1605a375"
 
-    # Country Code.
     gi = pygeoip.GeoIP("static/GeoIP.dat")  # Lookup for country codes. Obtained from http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 
+    # Get real client IP if behind proxy (like Heroku).
     if request.headers.getlist("X-Forwarded-For"):
-       ip = request.headers.getlist("X-Forwarded-For")[-1]
+        ip = request.headers.getlist("X-Forwarded-For")[-1]
     else:
-       ip = request.remote_addr
+        ip = request.remote_addr
 
-    # ip = request.headers.getlist("X-Forwarded-For")[-1] # Get real client IP.
-    print "headers.getlist('X-Forwarded-For') : " + str(ip)
-    # ip = "104.142.123.45"
-    # print "ip : " + ip
-
-    cc = "&country_code={}".format(gi.country_code_by_addr(ip))
-    # cc = "&country_code=US"
+    # Country Code. Default for localhost.
+    if gi.country_code_by_addr(ip):
+        cc = "&country_code={}".format(gi.country_code_by_addr(ip))
+    else:
+        cc = "&country_code=CA"
 
     # HTTP Post.
     base_url = "https://offers.pretio.in/publishers/{}/api/?user_id=12345{}".format(API_KEY, cc)
